15cc1334fa9942cc930f9dafc2b5a847c61d71e8
DERBY <derbyt.uni@gmail.com>
2024-06-24 23:57:20 +1000

    minor changes

--- test/seleniumtest/seleniumtest/Program.cs
+++ test/seleniumtest/seleniumtest/Program.cs
@@ -12,54 +12,45 @@ public static class Script
 
         try
         {
-            // Log message
             Console.WriteLine("Navigating to Home Page");
             driver.Navigate().GoToUrl("http://localhost:5173");
             System.Threading.Thread.Sleep(3000);
 
-            // Log message
             Console.WriteLine("Navigating to About Page");
             IWebElement aboutLink = driver.FindElement(By.XPath("//a[contains(text(), 'About')]"));
             aboutLink.Click();
             System.Threading.Thread.Sleep(3000);
 
-            // Log message
             Console.WriteLine("Clicking on the 'Vue' link");
             IWebElement vueLink = driver.FindElement(By.XPath("//a[contains(text(), 'Vue')]"));
             vueLink.Click();
             System.Threading.Thread.Sleep(3000);
 
-            // Log message
             Console.WriteLine("Clicking on the 'Jenkins' link");
             IWebElement jenkinsLink = driver.FindElement(By.XPath("//a[contains(text(), 'Jenkins')]"));
             jenkinsLink.Click();
             System.Threading.Thread.Sleep(3000);
 
-            // Log message
             Console.WriteLine("Clicking on the 'GitHub' link");
             IWebElement githubLink = driver.FindElement(By.XPath("//a[contains(text(), 'GitHub')]"));
             githubLink.Click();
             System.Threading.Thread.Sleep(3000);
 
-            // Log message
             Console.WriteLine("Clicking on the 'Selenium' link");
             IWebElement seleniumLink = driver.FindElement(By.XPath("//a[contains(text(), 'Selenium')]"));
             seleniumLink.Click();
             System.Threading.Thread.Sleep(3000);
 
-            // Log message
             Console.WriteLine("Clicking on the '000webhost.com' link");
             IWebElement webhostLink = driver.FindElement(By.XPath("//a[contains(text(), '000webhost.com')]"));
             webhostLink.Click();
             System.Threading.Thread.Sleep(3000);
 
-            // Log message
             Console.WriteLine("Clicking on the 'Publish over FTP' link");
             IWebElement ftpLink = driver.FindElement(By.XPath("//a[contains(text(), 'Publish over FTP')]"));
             ftpLink.Click();
             System.Threading.Thread.Sleep(3000);
 
-            // Log message
             Console.WriteLine("Tests completed successfully.");
         }
         finally
